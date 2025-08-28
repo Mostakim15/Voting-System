fb5c006d773890dc82f35bb0c385b9d239690e64
Mostakim15 <mbmostakim5050@gmail.com>
2025-08-28 23:09:15 +0600

    f

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-int pass;
-            // printf("Please enter your admin password must be integer:");
-            // scanf("%d",&pass);
-            // if (pass == admin){
-            //     viewResults(candidates, cCount);
-            // }
